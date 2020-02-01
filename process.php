eee61e45cdee5cc8956a441f6520515a405a17af
mik0why <mikowhy72@gmail.com>
2020-02-01 16:27:57 -0500

    working on gettingthe contact form to work

--- process.php
+++ process.php
@@ -2,18 +2,15 @@
 
 
     if(isset($_POST['submit'])){
-        $to = "mikowhy72@gmail.com";
-        $subject = $_POST['subject'];
-        $email = $_POST['email'];
-        $txt = $_POST['message'];
-        $headers = "From: ".$email . "\r\n".
-        "CC: somebodyelse@example.com";
 
-        mail($to, $subject, $txt, $headers);
-        header("Location: contact.html");        
+
+        echo "it works";
         
-        
-    }
+        }else{
+            echo "it doesn't work";
+        }
+
 
 
 ?>
+
