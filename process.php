db14b8e88a362ad008d1a5f1492959baca0badbf
mik0why <mikowhy72@gmail.com>
2020-02-06 09:00:11 -0500

    changes to fonts, updated process

--- process.php
+++ process.php
@@ -1,15 +1,29 @@
+
 <?php
 
 
     if(isset($_POST['submit'])){
 
 
-        echo "it works";
-        
+        $to = "mikowhy72@gmail.com";
+        $subject = $_POST['subject'];
+        $email = $_POST['email'];
+        $txt = $_POST['message'];
+        $headers = "From: ".$email . "\r\n";
+        /*header("Location: contact.html");       */ 
+      
+        mail($to, $subject, $txt, $headers);
+
+    
+        if(mail($to, $subject, $txt, $headers)){
+            echo "The form has been submitted.";
         }else{
-            echo "it doesn't work";
+            echo "The form has not been submitted.";
         }
 
+        
+        
+    }
 
 
 ?>
